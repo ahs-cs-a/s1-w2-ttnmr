519375267c22e8ecc9100e70d8d60fac8327759f
tt <tate27n@gmail.com>
2025-08-22 13:46:57 -0700

    body of water

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,29 @@
+public class BodyOfWater {
+    private String name;
+    private int avgDepth;
+    private int largestDiameter;
+    private boolean isSaltWater;
 
+    public BodyOfWater (String n, int a) {
+        name = n;
+        avgDepth = a; //meters
+    }
+
+    public BodyOfWater (String n, int a, int l, boolean i) {
+        name = n;
+        avgDepth = a; //meters
+        largestDiameter = l; //kilometers
+        isSaltWater = i;
+    }
+
+    public void setName (String n) {
+        name = n;
+    }
+
+    public void setLargestDiameter (int l) {
+        largestDiameter = l;
+    }
+     public void printState() {
+        System.out.println("Name: " + name + "\nLargest Diameter: ");
+     }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,9 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
+        BodyOfWater b1 = new BodyOfWater("Lake Tahoe", 304);
+        BodyOfWater b = new BodyOfWater("Lake Superior", 147, 560, false);
         System.out.println("here");
+        b.printState();
+        b1.printState();
     }
 }
